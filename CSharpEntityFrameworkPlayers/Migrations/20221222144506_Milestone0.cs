d99a98c765e72bbba152c8516144f5122c544c6f
robertolagreca <robertolagreca92@gmail.com>
2022-12-22 15:46:02 +0100

    Creazione DB - Milestone 0 completata

---
+++ CSharpEntityFrameworkPlayers/Migrations/20221222144506_Milestone0.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSharpEntityFrameworkPlayers.Migrations
+{
+    /// <inheritdoc />
+    public partial class Milestone0 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "player",
+                columns: table => new
+                {
+                    PlayerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    score = table.Column<int>(type: "int", nullable: false),
+                    MatchesPlayed = table.Column<int>(type: "int", nullable: false),
+                    MatchesWon = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_player", x => x.PlayerId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "player");
+        }
+    }
+}
---
+++ CSharpEntityFrameworkPlayers/PlayerContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEntityFrameworkPlayers
+{
+    public class PlayerContext: DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=SportPlayer;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+    }
+
+    
+}
