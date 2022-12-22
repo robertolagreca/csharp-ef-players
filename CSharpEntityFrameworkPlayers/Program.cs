37c73f9ea9a54fa9fa3a78d785e048510dd31f01
robertolagreca <robertolagreca92@gmail.com>
2022-12-22 16:21:42 +0100

    Milestone 2 - Escluse funzioni generazione numeri random e check

--- CSharpEntityFrameworkPlayers/Program.cs
+++ CSharpEntityFrameworkPlayers/Program.cs
@@ -4,19 +4,23 @@ using CSharpEntityFrameworkPlayers;
 using (PlayerContext db = new PlayerContext())
 {
     Player player1 = new Player() { Name = "Giovanni", Surname = "Baglioni", Score = 10, MatchesPlayed = 10, MatchesWon = 10 };
+    //Add
     db.Add(player1);
     db.SaveChanges();
     
     Console.WriteLine("Reading dopo aggiunta players");
+    //Read
     List<Player> players = db.Players.OrderBy(player => player.Name).ToList<Player>();
     Console.WriteLine();
 
     Console.WriteLine("Modifica di un record in corso");
+    //Update
     player1.Name = "Veronica";
     Console.WriteLine(players);
     db.SaveChanges();
     Console.WriteLine();
 
+    //Remove
     db.Remove(player1);
     db.SaveChanges();
 
