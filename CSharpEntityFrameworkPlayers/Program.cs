1f5f7c88f4c0cc6852fa19b5c1dc87744c26a877
robertolagreca <robertolagreca92@gmail.com>
2022-12-22 16:20:31 +0100

    Milestone 1 - Escluse funzioni generazione numeri random e check

--- CSharpEntityFrameworkPlayers/Program.cs
+++ CSharpEntityFrameworkPlayers/Program.cs
@@ -1,26 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CSharpEntityFrameworkPlayers;
 
-using (PlayerContext db= new PlayerContext())
+using (PlayerContext db = new PlayerContext())
 {
-    Player player1 = new Player() { Name = "Aldo", Surname = "Baglio", Score = 10, MatchesPlayed = 10, MatchesWon = 10 };
+    Player player1 = new Player() { Name = "Giovanni", Surname = "Baglioni", Score = 10, MatchesPlayed = 10, MatchesWon = 10 };
     db.Add(player1);
     db.SaveChanges();
-    Console.WriteLine("E' stato registrato con successo Nome: " + player1.Name);
-}
+    
+    Console.WriteLine("Reading dopo aggiunta players");
+    List<Player> players = db.Players.OrderBy(player => player.Name).ToList<Player>();
+    Console.WriteLine();
 
-/*using (SchoolContext db = new SchoolContext())
-{
-    CourseImage corsoImmagine = new CourseImage() { Caption = "Corso per diventare info", ImageRoute = "Path immagine" };
-    Course corsoNuovo = new Course() { ImageCourse = corsoImmagine, Name = "Informatica" };
-    Student studenteMate = new Student() { Name = "Ver", Surname = "Cia", Email = "dffdf", AttendedCourses = new List<Course>() };
-
-    //corsoNuovo.ImageCourse= corsoImmagine; inserito già sopra
-    studenteMate.AttendedCourses.Add(corsoNuovo);
-
-
-    db.Add(corsoImmagine);
-    db.Add(corsoNuovo);
-    db.Add(studenteMate);
+    Console.WriteLine("Modifica di un record in corso");
+    player1.Name = "Veronica";
+    Console.WriteLine(players);
     db.SaveChanges();
-} */
+    Console.WriteLine();
+
+    db.Remove(player1);
+    db.SaveChanges();
+
+}
